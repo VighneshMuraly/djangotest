8e2533421dd9ec4d7501fc76d815d67fe5ebf0c1
pigisbuff <55902778+pigisbuff@users.noreply.github.com>
2021-05-31 15:29:54 +0530

    Add files via upload

---
+++ djangoapp/Analytics/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+# class NameForm(forms.Form):
+#     firstname = forms.CharField(label='fname', max_length=100)
+#     lastname = forms.CharField(label='lname', max_length=100)
+#     adress = forms.CharField(label='adress', max_length=100)
+#     firstname = forms.CharField(label='phone', max_length=10)
+#     firstname = forms.CharField(label='cname', max_length=100)
+#     firstname = forms.CharField(label='cpos', max_length=100)
